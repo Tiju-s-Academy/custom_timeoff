bc2c87585e62856ae3f78770f6284bb67e424ded
Adil <adilpk123@gmail.com>
2024-11-25 10:24:15 +0530

    deleted inherited employee module

--- __manifest__.py
+++ __manifest__.py
@@ -2,10 +2,8 @@
     'name': 'custom timeoff',
     'version': '17.0.1.0.0',
     'summary': 'add new type approvers',
-    'depends': ['base','web','mail','hr_holidays','hr'],
+    'depends': ['base','web','mail','hr_holidays'],
     'data': [
-        'security/groups.xml',
-        'security/record_rule.xml',
         'views/hr_leave_type_inherit_view.xml',
         'views/hr_leave_view_inherit.xml',
     ],
